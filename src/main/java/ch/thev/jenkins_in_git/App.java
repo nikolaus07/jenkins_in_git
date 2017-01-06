a9db56b6ef67f846d05c793e1380361d5f61df17
Klaus Thevessen <nikolaus07@users.noreply.github.com>
2017-01-06 15:41:18 +0100

    absichtlich ein fehler eingebaut in .java

--- src/main/java/ch/thev/jenkins_in_git/App.java
+++ src/main/java/ch/thev/jenkins_in_git/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "hallo git. Hier JENKINS" );
+        Sys   xxxx  tem.out.println( "hallo git. Hier JENKINS" );
     }
 }
